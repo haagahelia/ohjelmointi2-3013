15c2ad6c3e8afafb24ee2f2494f30e038d29302a
TH <h01581@haaga-helia.fi>
2020-03-24 09:33:56 +0200

    Tomcat-version palautus 9:ään ja JSTL-kirjaston lisääminen.

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -2,8 +2,8 @@ package launch;
 
 import java.io.File;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
@@ -25,26 +25,29 @@ public class Main {
         Tomcat tomcat = new Tomcat();
 
         // Asetetaan Tomcatin HTTP-portti. Jos "PORT" löytyy ympäristömuuttujista,
-        // käytetään sitä. Muussa tapauksessa portti 8080:
-        String webPort = System.getenv().getOrDefault("PORT", "8080");
-        tomcat.setPort(Integer.valueOf(webPort));
+        // käytetään sitä. Muussa tapauksessa käytetään porttia 8080:
+        String httpPort = System.getenv().getOrDefault("PORT", "8080");
+        tomcat.setPort(Integer.valueOf(httpPort));
 
         // Luodaan Connector-olio, joka kuuntelee asettamaamme porttia:
         tomcat.getConnector();
 
-        // Tomcatin asetukset:
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", webappDirPath);
+        // Lisätään oma sovelluksemme Tomcatiin palvelimen juureen:
+        Context webApp = tomcat.addWebapp("/", webappDirPath);
 
-        // Käännettyjen Java-koodien sijainnit "WEB-INF/classes"-hakemiston lisäksi:
-        File additionWebInfClasses = new File("target/classes");
-        WebResourceRoot resources = new StandardRoot(ctx);
+        // HUOM! Jos haluat, että palvelin käynnistää itsensä uudelleen muutettuasi
+        // tiedostoja, poista kommentti seuraavalta riviltä:
+        // webApp.setReloadable(true);
+
+        // Määritellään sovelluksemme resurssien sijainnit:
+        WebResourceRoot resources = new StandardRoot(webApp);
         resources.addPreResources(
-                new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
-        ctx.setResources(resources);
+                new DirResourceSet(resources, "/WEB-INF/classes", new File("target/classes").getAbsolutePath(), "/"));
+        webApp.setResources(resources);
 
         // Asetetaan UTF-8 -merkistö HTTP-pyyntöihin ja -vastauksiin:
-        ctx.setRequestCharacterEncoding("utf-8");
-        ctx.setResponseCharacterEncoding("utf-8");
+        webApp.setRequestCharacterEncoding("utf-8");
+        webApp.setResponseCharacterEncoding("utf-8");
 
         // Käynnistetään palvelin ja odotetaan yhteyksiä:
         tomcat.start();
